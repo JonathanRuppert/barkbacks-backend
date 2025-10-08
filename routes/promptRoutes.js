d1b6678a68594f809bb1dc8ebd04fbc8d1602225
Jonathan Ruppert. <jon@barkbacks.com>
2025-10-08 14:57:42 -0400

    Aligned /api/prompt route with frontend

--- routes/promptRoutes.js
+++ routes/promptRoutes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { generatePrompt } = require('../controllers/promptController');
 
-router.post('/generate-prompt', generatePrompt);
+router.post('/api/prompt', generatePrompt);
 
 module.exports = router;
