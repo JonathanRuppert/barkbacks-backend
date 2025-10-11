7b93ab02a9c64444fe4782a7dfff9b7cead3d0a9
Jonathan Ruppert. <jon@barkbacks.com>
2025-10-11 18:25:30 -0400

    Update server.js and fix MongoDB URI

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 
 // MongoDB Connection
+mongoose.set('strictQuery', false); // Optional but recommended for compatibility
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
