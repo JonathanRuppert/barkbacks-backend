37f06f31a4ce096957240a04da86d00e2d21b1e8
Jonathan Ruppert. <jon@barkbacks.com>
2025-10-24 15:29:37 -0400

    🧼 Fixed syntax error on http import

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const http = require('http'); & voicc
+const http = require('http');
 const WebSocket = require('ws');
 const Story = require('./models/storyModel');
 
